e101e9d68681897923dfe81e710311504790633b
DevilIsDead <kash.izu333@gmail.com>
2023-05-12 14:41:52 +0300

    sdasd

--- C3_SimpleBashUtils-1-master/src/grep/grep.c
+++ C3_SimpleBashUtils-1-master/src/grep/grep.c
@@ -98,7 +98,7 @@ int grep (char *filepath, options config) {
         regfree(&regex);
       }
     }
-    if (config.l || config.c) printf("%s:", filepath);
+    if (config.l || config.c) printf("%s", filepath);
     if (config.c && !config.l) printf("%d\n", config.goodLineCount);
     if (config.l) printf("\n");
     config.lineCount = 0;
