0c6d9f982c29ec81076f3adb543153a6b9d02c17
unknown <kash.izu333@gmail.com>
2023-06-02 23:25:05 +0300

    123

--- string_h/main.c
+++ string_h/main.c
@@ -4,10 +4,11 @@
 
 int main () {
     int err = 0;
-    char *str1 = "012345 At the time of Stephen Wolfram's release announcement for Mathematica 13, there were 2,259 functions contributed as Resource Functions.";
-    char *str2 = "At the time of 6Stephen Wolfram's release announcement for Mathematica 13, there were 2,259 functions contributed as Resource Functions.";
-    char *str3 = "0123456789";
-    char str4[50] = {'\0'};
+    char str1[143] = "012345 At the time of Stephen Wolfram's release announcement for Mathematica 13, there were 2,259 functions contributed as Resource Functions.";
+    char str2[149] = "At the time of 6Stephen for Wolfram's release announcement for Mathematica 13, there were 2,259 for  forfunctions contributed as Resource Functions.";
+    char str3[44] = "0123456789|0123456789|0123456789|0123456789";
+    char str4[100] = {'\0'};
+    char *delim = " ";
     printf("str1 len = %ld\n", s21_strlen(str1));
     printf("str2 len = %ld\n", s21_strlen(str2));
     printf("str3 len = %ld\n", s21_strlen(str3));
@@ -17,6 +18,10 @@ int main () {
     printf("str1 + str3 : %s\n", res);
     res = s21_strncat(str4, str3, 5);
     printf("str1 + str3 5 syms : %s\n", res);
-
+    res = strtok(str2, delim);
+    while (res != NULL) {
+        printf("strtok str3 : %s\n", res);
+        res = strtok(NULL, delim);
+    }
     return err;
 }
--- string_h/s21_string.h
+++ string_h/s21_string.h
@@ -1,6 +1,16 @@
 #ifndef S21_STRING_H_
 #define S21_STRING_H_
 
+#define BUFF_SIZE 512
+
+#if defined(__APPLE__) || defined(__MACH__)
+#define S21_ERRLIST_SIZE 107
+#elif defined(__linux__)
+#define S21_ERRLIST_SIZE 134
+#else
+#define S21_ERRLIST_SIZE 134
+#endif
+
 typedef unsigned long s21_size_t;
 #define S21_NULL ((void *)0)
 
@@ -9,5 +19,6 @@ s21_size_t s21_strspn(const char *str1, const char *str2);
 s21_size_t s21_strcspn(const char *str1, const char *str2);
 char *s21_strcat(char *dest, const char *src);
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
+char *s21_strerror(int errnum);
 
 #endif // S21_STRING_H_
