88c4d0b1517db0307785515ab579a343e83a7d68
unknown <kash.izu333@gmail.com>
2023-06-06 15:49:32 +0300

    123

--- string_h/s21_strstr.c
+++ string_h/s21_strstr.c
@@ -6,10 +6,10 @@ char *s21_strstr(const char *haystack, const char *needle) {
     int len2 = s21_strlen(needle);
     if (len1 >= len2) {
         for (int i = 0; i <= len1 - len2; i++) {
-            int found = 0;
-            for (int j = i, k = 0; needle[k]; k++, j++) {
-                if (haystack[j] == needle[k]) {
-                    found = 1;
+            int found = 1;
+            for (int k = 0; k < len2; k++) {
+                if (haystack[i+k] != needle[k]) {
+                    found = 0;
                     break;
                 }
             }
