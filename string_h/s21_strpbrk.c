22e07d03313a0157bc5c907ebe033500e97812e5
unknown <kash.izu333@gmail.com>
2023-06-06 11:49:30 +0300

    123

--- string_h/s21_strpbrk.c
+++ string_h/s21_strpbrk.c
@@ -2,10 +2,12 @@
 
 char *s21_strpbrk(const char *str1, const char *str2) {
     char *res = S21_NULL;
-    for (int i = 0; i < s21_strlen(str1); i++) {
-        for (int j = 0; j < s21_strlen(str2); j++) {
+    int len1 = s21_strlen(str1);
+    int len2 = s21_strlen(str2);
+    for (int i = 0; i < len1; i++) {
+        for (int j = 0; j < len2; j++) {
             if (str1[i] == str2[j]) {
-                res = (char*)str1[i];
+                res = &str1[i];
                 break;
             }
         }
